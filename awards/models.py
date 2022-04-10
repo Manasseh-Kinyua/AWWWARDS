b384b40a4dee4ab01df27bd81b579b585496a7a4
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-10 13:58:53 +0300

    migarate the database to use the models

--- awards/models.py
+++ awards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,6 +8,19 @@ class Project(models.Model):
     title = models.CharField(max_length = 100)
     image = models.ImageField(upload_to = 'images/')
     descriptioin = models.TextField(null = True)
+    profile = models.ForeignKey("Profile", on_delete = models.CASCADE, null = True)
     link = models.CharField(max_length = 2000)
     id = models.UUIDField(default = uuid.uuid4, unique = True, primary_key = True, editable = False)
     posted = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'images/')
+    bio = models.TextField(null=True)
+    phone_no = models.IntegerField()
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.user
