99d722df762109f309fc28048adc45e5cfbfce6c
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-10 13:42:57 +0300

    install pillow

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length = 100)
+    image = models.ImageField(upload_to = 'images/')
+    descriptioin = models.TextField(null = True)
+    link = models.CharField(max_length = 2000)
+    id = models.UUIDField(default = uuid.uuid4, unique = True, primary_key = True, editable = False)
+    posted = models.DateTimeField(auto_now_add = True)
