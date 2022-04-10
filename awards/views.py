cd188ddcb29f98e7ac0ff09828f76054b4b6e9f5
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-10 15:44:01 +0300

    Add the login functionality

--- awards/views.py
+++ awards/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Project
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
     context = {}
     return render(request, 'awards/login_register.html', context)
 
