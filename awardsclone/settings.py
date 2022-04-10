195a091bce3b06379994ae4dda05c0a56fb4ae91
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-10 13:13:28 +0300

    install psycopg2 and configure settings to use postgresql

--- awardsclone/settings.py
+++ awardsclone/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'awardsclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'nasseh',
+        'PASSWORD': 'nasseh82473',
     }
 }
 
